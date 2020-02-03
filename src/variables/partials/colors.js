24f15bfca80d7c4b9b02b23316dc9a450299463b
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-02-03 13:43:12 -1000

    creates variables for styled comopnents

---
+++ src/variables/partials/colors.js
@@ -0,0 +1,8 @@
+export const white = '#fefcf6';
+export const black = '#2D2926';
+export const grey = 'grey';
+export const dark_blue = '#0c363c';
+export const pink = '#e24363';
+export const yellow = '#efd46b';
+export const brown = '#c6944c';
+export const light_blue = '#84c9b8'

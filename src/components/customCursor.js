1634e4a312c442dbed83316be040060c4c1df381
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-02-05 20:40:25 -1000

    breaks out cursor into styled component

--- src/components/customCursor.js
+++ src/components/customCursor.js
@@ -1,37 +1,23 @@
 import React from "react"
-import styled from "styled-components"
-import { breakpointMedium } from "../variables"
+import { Cursor } from "../elements"
 
 const CustomCursor = ({ xValue, yValue, cursorElement }) => {
-  return (
-    <Cursor
-      top={yValue}
-      left={xValue}
-      height="20"
-      width="100"
-      zIndex="99999999999"
-    >
-      {cursorElement}
-    </Cursor>
-  )
+  switch (cursorElement) {
+    case "initial":
+      return (
+        <Cursor
+          top={yValue}
+          left={xValue}
+          height="20"
+          width="100"
+          zIndex="99999999999"
+        >
+          initial
+        </Cursor>
+      )
+    default:
+      return null
+  }
 }
 
 export default CustomCursor
-
-const Cursor = styled.div`
-  position: fixed;
-  display: none;
-  place-items: center;
-  z-index: ${props => props.zIndex};
-  height: ${props => props.height}px;
-  width: ${props => props.width}px;
-  border-radius: ${props => props.borderRadius};
-  pointer-events: none;
-  transform: ${props => `translate(${props.left}px, ${props.top}px)`};
-  left: ${props => `-${props.width / 2}px`};
-  top: ${props => `-${props.height / 2}px`};
-
-  @media (min-width: ${breakpointMedium}) {
-    display: grid;
-  }
-`

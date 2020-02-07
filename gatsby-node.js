0422ef6466fa3374c940fe11e5fc69ace7436e2c
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-02-06 16:18:23 -1000

    creates new pages in gastby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,66 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const pages = await graphql(`
+    {
+      allPrismicProjectTemplate {
+        edges {
+          node {
+            id
+            uid
+          }
+          next {
+            uid
+            id
+            data {
+              project_name {
+                text
+              }
+              featured_image {
+                localFile {
+                  childImageSharp {
+                    fixed {
+                      srcSet
+                    }
+                  }
+                }
+              }
+            }
+          }
+          previous {
+            uid
+            id
+            data {
+              project_name {
+                text
+              }
+              featured_image {
+                localFile {
+                  childImageSharp {
+                    fixed {
+                      srcSet
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  pages.data.allPrismicProjectTemplate.edges.forEach(edge => {
+    createPage({
+      path: `/${edge.node.uid}`,
+      component: path.resolve("./src/templates/project.js"),
+      context: {
+        id: edge.node.id,
+        previous: edge.previous,
+        next: edge.next,
+      },
+    })
+  })
+}

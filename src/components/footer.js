6f14c291351623ccc301a12585bc53e71927b94e
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-02-07 13:25:33 -1000

    restyles the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,24 @@
 import React from "react"
 import styled from "styled-components"
-import { breakpointSmall } from "../variables"
+import { breakpointSmall, breakpointMedium } from "../variables"
 
 const Footer = () => {
   return (
     <FooterContent>
       <img src={"/contact-us.svg"} alt="contact us call to action" />
       <ContactInfo>
-        Maui 2261 Aupuni Street #101 Wailuku, HI 96793 808.249.2200
+        <p>Maui 2261 Aupuni Street #101 Wailuku, HI 96793</p>
+        <p>808.249.2200</p>
       </ContactInfo>
       <ContactInfo>
-        Oahu 555 South Street #108 Honolulu, HI 96813 808.544.0002
+        <p>Oahu 555 South Street #108 Honolulu, HI 96813</p>
+        <p>808.544.0002</p>
       </ContactInfo>
       <ContactInfo className="center">
-        info@saedesign.com insta | bonus
+        <p>
+          <span>info@saedesign.com</span> <span>insta</span> |{" "}
+          <span>bonus</span>
+        </p>
       </ContactInfo>
     </FooterContent>
   )
@@ -25,23 +30,37 @@ const FooterContent = styled.footer`
   padding: 20px;
 
   @media (min-width: ${breakpointSmall}) {
-    padding: 64px;
     display: flex;
     justify-content: space-between;
     align-items: flex-end;
+  }
+
+  @media (min-width: ${breakpointMedium}) {
+    padding: 64px;
   }
 `
 
 const ContactInfo = styled.div`
   font-weight: 700;
-  width: 236px;
-  font-size: 20px;
-  line-height: 1.9;
+  font-size: 16px;
 
-  &.center {
-    height: 126px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  p {
+    margin: 0;
+    line-height: 1.9;
+  }
+
+  @media (min-width: ${breakpointSmall}) {
+    width: 236px;
+
+    &.center {
+      height: 126px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+  }
+
+  @media (min-width: ${breakpointMedium}) {
+    font-size: 20px;
   }
 `

bfd3103687014987fbcdc1a7410e5a0b0a7649df
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-02-06 17:29:04 -1000

    refactors links to fit the transition-link

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,19 +1,32 @@
 import React from "react"
 import Layout from "../components/layout"
-import { Link } from "gatsby"
+import Link from "gatsby-plugin-transition-link"
 
 const Project = ({ pageContext }) => {
   const { next, previous } = pageContext
   const { project_name: nextName } = next.data
   const { project_name: prevName } = previous.data
+  const {
+    src: prevImage,
+  } = previous.data.featured_image.localFile.childImageSharp.fixed
+  const {
+    src: nextImage,
+  } = next.data.featured_image.localFile.childImageSharp.fixed
 
   return (
     <Layout>
-      <div>this is a project</div>
+      <h1>project</h1>
       <h3>previous</h3>
-      <Link to={`/${previous.uid}`}>{prevName.text}</Link>
+      <Link to={`/${previous.uid}`}>
+        <img src={prevImage} alt={prevName.text} />
+        <p>{prevName.text}</p>
+      </Link>
       <h3>next</h3>
-      <Link to={`/${next.uid}`}>{nextName.text}</Link>
+
+      <Link to={`/${next.uid}`}>
+        <img src={nextImage} alt={nextName.text} />
+        <p>{nextName.text}</p>
+      </Link>
     </Layout>
   )
 }

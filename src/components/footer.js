cb7b2ce1f7b5547b01112b83f97e614db00c4ca6
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-02-07 12:46:06 -1000

    adds the footer illustration

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={"/contact-us.svg"} alt="contact us call to action" />
+      footer
+    </footer>
+  )
+}
+
+export default Footer

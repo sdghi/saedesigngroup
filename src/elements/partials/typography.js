ec611b5555c471929f9a66c2ee325939f2778de6
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-02-10 21:14:54 -1000

    adjust styled-components of typography

--- src/elements/partials/typography.js
+++ src/elements/partials/typography.js
@@ -5,6 +5,7 @@ export const Paragraph = styled.p`
   font-size: ${props => props.fontSize};
   line-height: ${props => props.lineHeight};
   text-align: ${props => props.textAlign};
+  margin-top: ${props => props.top};
 
   @media (min-width: ${breakpointSmall}) {
     font-size: ${props => props.fontSizeMd};

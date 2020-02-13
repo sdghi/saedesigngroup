7674e1c460b63540e2d05dc7e4cfb2a26f14716f
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-02-12 23:32:08 -1000

    refactors the cursor to be more perfomant

--- src/elements/partials/cursor.js
+++ src/elements/partials/cursor.js
@@ -1,18 +1,21 @@
 import styled from "styled-components"
 import { breakpointMedium } from "../../variables"
 
-export const Cursor = styled.div`
+export const Cursor = styled.div.attrs(props => ({
+  style: {
+    zIndex: props.zIndex,
+    width: `${props.width}px`,
+    height: `${props.height}px`,
+    borderRadius: props.borderRadius,
+    left: `-${props.width / 2}px`,
+    top: `-${props.height / 2}px`,
+    transform: `translate(${props.left}px, ${props.top}px)`,
+  },
+}))`
   position: fixed;
   display: none;
   place-items: center;
-  z-index: ${props => props.zIndex};
-  height: ${props => props.height}px;
-  width: ${props => props.width}px;
-  border-radius: ${props => props.borderRadius};
   pointer-events: none;
-  transform: ${props => `translate(${props.left}px, ${props.top}px)`};
-  left: ${props => `-${props.width / 2}px`};
-  top: ${props => `-${props.height / 2}px`};
 
   @media (min-width: ${breakpointMedium}) {
     display: grid;

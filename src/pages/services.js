63c98e510a1032a6462e43f11e0f78c75e56f920
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-03-03 23:15:45 -1000

    queries the main menu section

--- src/pages/services.js
+++ src/pages/services.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { myContext } from "../provider"
+import styled from 'styled-components'
 
 export const query = graphql`
 {
@@ -52,19 +53,51 @@ export const query = graphql`
 }
 `
 
-const ServicesPage = () => {
+const ServicesPage = ({ data }) => {
   const { setCursorElement } = useContext(myContext)
+
+  const { body: mainMenu, add_a_side_of, design_audit, we_do_it_all } = data.allPrismicMenuOfServices.edges[0].node.data;
+
 
   useEffect(() => {
     setCursorElement({ initial: "initial" })
+    console.log({ mainMenu, add_a_side_of, design_audit, we_do_it_all })
   }, [setCursorElement])
 
   return (
     <Layout>
       <SEO title="Services" />
-      <div>Services</div>
+      <MainMenuSection>
+        {mainMenu.map(service => (
+          <div key={service.id}>
+            <h2>{service.primary.title.text}</h2>
+
+            {service.items.map((item, index) => (
+              <div key={index}>
+                <h3>{item.feature_title.text}</h3>
+                <p>{item.feature_description.text}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+        <div className="add-a-side-of">
+          <h2>Add A Side Of</h2>
+          {add_a_side_of.map((side, index) => (
+            <p key={index}>{side.title.text}</p>
+          ))}
+        </div>
+      </MainMenuSection>
     </Layout>
   )
 }
 
 export default ServicesPage
+
+const MainMenuSection = styled.section`
+  padding: 20px;
+  border: 1px solid red;
+
+  .add-a-side-of{
+    border: 1px solid green;
+  }
+`;

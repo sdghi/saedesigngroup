2bc9b4952b300e33817943d1f25874c6f7d4fac1
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-03-03 23:50:13 -1000

    styles the mobile version of main menu section

--- src/pages/services.js
+++ src/pages/services.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { myContext } from "../provider"
 import styled from 'styled-components'
+import { light_blue, dark_blue, yellow, pink } from '../variables'
 
 export const query = graphql`
 {
@@ -61,7 +62,6 @@ const ServicesPage = ({ data }) => {
 
   useEffect(() => {
     setCursorElement({ initial: "initial" })
-    console.log({ we_do_it_all })
   }, [setCursorElement])
 
   return (
@@ -74,8 +74,8 @@ const ServicesPage = ({ data }) => {
               <h2>{service.primary.title.text}</h2>
 
               {service.items.map((item, index) => (
-                <div key={index}>
-                  <h3>{item.feature_title.text}</h3>
+                <div key={index} className="service-item">
+                  <h3 className="service-title">{item.feature_title.text}</h3>
                   <p>{item.feature_description.text}</p>
                 </div>
               ))}
@@ -113,23 +113,76 @@ const ServicesPage = ({ data }) => {
 export default ServicesPage
 
 const MenuContainer = styled.div`
+  padding: 18.5px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 30px;
 `;
 
 const MainMenu = styled.section`
-  padding: 20px;
-  border: 1px solid red;
+  padding: 43px 30px 30px 30px;
+  border: 4px solid ${dark_blue};
+  border-radius: 19.41px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 20px;
+
+  h2{
+    color: ${light_blue};
+    font-size: 26px;
+    font-weight: 500;
+    letter-spacing: 0.153em; 
+    margin: 0 0 18px 0;
+    text-transform: uppercase;
+  }
+
+  .service-item{
+    margin-bottom: 18px;
+
+    .service-title{
+      font-size: 13px;
+      letter-spacing: 0.2em;
+      font-weight: 900;
+      margin: 0;
+      text-transform: uppercase;
+    }
+
+    p{
+      margin: 0;
+      font-size: 13px;
+      font-weight: 300;
+    }
+  }
 
   .add-a-side-of{
-    border: 1px solid green;
+    border: 4px solid ${yellow};
+    border-radius: 13px;
+    padding: 27px;
+
+    h2{
+      font-size: 18px;
+      font-weight: 700;
+      margin: 0;
+      color: ${yellow};
+    }
+
+    p{
+      margin: 0;
+      line-height: 2.1;
+      font-size: 13px;
+      font-weight: 700;
+    }
   }
 `;
 
 const DesignAudit = styled.section`
   padding: 20px;
-  border: 1px solid blue;
+  border: 4px solid ${pink};
+  border-radius: 19.41px;
 `;
 
 const WeDoItAll = styled.section`
   padding: 20px;
-  border: 1px solid orange;
+  border: 4px solid ${dark_blue};
+  border-radius: 19.41px;
 `;

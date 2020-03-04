16e7377505ad3081fed16f5a5c119f0ef62f893e
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-03-03 23:02:05 -1000

    queries data from the menu of services api in cms

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,7 +1,56 @@
 import React, { useEffect, useContext } from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { myContext } from "../provider"
+
+export const query = graphql`
+{
+  allPrismicMenuOfServices {
+    edges {
+      node {
+        data {
+       		body{
+            ... on PrismicMenuOfServicesBodyMainMenuItem{
+              primary{
+                title{
+                  text
+                }
+              }
+              items{
+                feature_title{
+                  text
+                }
+                feature_description{
+                  text
+                }
+              }
+            }
+          }
+          add_a_side_of {
+            title {
+              text
+            }
+          }
+          design_audit {
+            title {
+              text
+            }
+            description {
+              text
+            }
+          }
+          we_do_it_all {
+            industries {
+              text
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
 
 const ServicesPage = () => {
   const { setCursorElement } = useContext(myContext)

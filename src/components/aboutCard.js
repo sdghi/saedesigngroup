abdf1a772c926f067f074ca744d196768af5d2c7
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-03-10 18:21:55 -1000

    adds cards with tilt

---
+++ src/components/aboutCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import { white } from '../variables'
+import Tilt from "react-parallax-tilt";
+
+const AboutCard = () => {
+    return (
+        <Tilt >
+            <Card />
+        </Tilt>
+    )
+}
+
+export default AboutCard;
+
+const Card = styled.div`
+    background: ${white};
+    height: 100%;
+    width: 100%;
+`;
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,10 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { myContext } from "../provider"
+import styled from 'styled-components'
+import { yellow } from "../variables"
+import { motion } from 'framer-motion'
+import AboutCard from '../components/aboutCard'
 
 export const query = graphql`
   {
@@ -22,6 +26,8 @@ export const query = graphql`
     }
   }
 `
+// Comment me out once about content is populated
+const people = ['david', 'arlyn', 'ian', 'louis', 'sae', 'steve', 'leeann', 'judith', 'rachelle', 'marissa', 'kitty', 'patrick', 'leeann']
 
 const AboutPage = ({ data }) => {
   const { title, page_description } = data.allPrismicAbout.edges[0].node.data
@@ -35,10 +41,32 @@ const AboutPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="About" />
-      <h1>{title.text}</h1>
-      <p>{page_description.text}</p>
+      <AboutHero>
+        <motion.div drag dragConstraints={{ top: -150, left: -200, right: 200, bottom: 150 }} className="grid-container">
+          {people.map((person, i) => (
+            <AboutCard key={i} />
+          ))}
+        </motion.div>
+      </AboutHero>
     </Layout>
   )
 }
 
 export default AboutPage
+
+const AboutHero = styled.section`
+  height: calc(100vh - 7vh);
+  width: 100%;
+  background: ${yellow};
+  overflow: hidden;
+  display: grid;
+  place-content: center;
+
+  .grid-container{
+    height: 120vh;
+    width: 120vw;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 50px;
+  }
+`;

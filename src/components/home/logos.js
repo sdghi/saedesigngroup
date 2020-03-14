2d3fc5d3accfdecbc128d5f29e192871d292615a
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-03-14 12:35:11 -1000

    deletes commented code

--- src/components/home/logos.js
+++ src/components/home/logos.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import { Container, ImageContainer } from "../../elements"
 import { breakpointSmall, breakpointMedium, pink } from "../../variables"

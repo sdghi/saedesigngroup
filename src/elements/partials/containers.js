5372136734c9524a65b39e75d091393b1920a397
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-03-24 14:32:02 -1000

    edit: destructures props from containers styled components

--- src/elements/partials/containers.js
+++ src/elements/partials/containers.js
@@ -3,25 +3,26 @@ import { breakpointSmall } from "../../variables"
 import Img from "gatsby-image/withIEPolyfill"
 
 export const Container = styled.section`
-  padding: ${props => (props.padding ? props.padding : "0 5%")};
-  max-width: ${props => props.maxWidth};
-  width: ${props => props.width};
-  margin: ${props => (props.margin ? props.margin : "0 auto 20vh auto")};
+  padding: ${({ padding }) => padding ? padding : "0 5%"};
+  max-width: ${({ maxWidth }) => maxWidth};
+  width: ${ ({ width }) => width};
+  margin: ${ ({ margin }) => margin ? margin : "0 auto 20vh auto"};
 
-  @media (min-width: ${breakpointSmall}) {
-    padding: ${props => (props.paddingMd ? props.paddingMd : props.padding)};
-  }
+@media(min-width: ${ breakpointSmall}) {
+  padding: ${ ({ paddingMd, padding }) => paddingMd ? paddingMd : padding};
+  margin: ${ ({ marginMd, margin }) => marginMd ? marginMd : margin};
+}
 `
 
 export const ImageContainer = styled(Img)`
-  width: ${props => (props.width ? props.width : "100%")};
-  height: ${props => (props.height ? props.height : "300px")};
-  max-width: ${props => (props.maxWidth ? props.maxWidth : "1781.99px")};
-  margin: ${props => (props.margin ? props.margin : "0 auto")};
+  width: ${ ({ width }) => width ? width : "100%"};
+  height: ${ ({ height }) => height ? height : "300px"};
+  max-width: ${({ maxWidth }) => maxWidth ? maxWidth : "1781.99px"};
+  margin: ${ ({ margin }) => margin ? margin : "0 auto"};
 
-  @media (min-width: ${breakpointSmall}) {
-    height: ${props => (props.heightMd ? props.heightMd : "80vh")};
-    width: ${props => (props.widthMd ? props.widthMd : "100%")};
-    margin: ${props => (props.marginMd ? props.marginMd : "0 auto")};
-  }
+@media(min-width: ${ breakpointSmall}) {
+  height: ${ ({ heightMd }) => heightMd ? heightMd : "80vh"};
+  width: ${ ({ widthMd }) => widthMd ? widthMd : "100%"};
+  margin: ${ ({ marginMd }) => marginMd ? marginMd : "0 auto"};
+}
 `

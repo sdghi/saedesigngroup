c149dbc8eb54887678133e4147982247770efa49
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-04-07 21:21:12 -1000

    feat: adds switching for the we are section

--- src/components/about/weAreSection.js
+++ src/components/about/weAreSection.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Paragraph } from '../../elements'
 import styled from 'styled-components'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const WeAreSection = ({ weAre }) => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const countChange = setInterval(() => {
+            if (count === weAre.length - 1) {
+                setCount(0);
+            } else {
+                setCount(count + 1)
+            }
+
+        }, [1000])
+
+        return () => { clearInterval(countChange) }
+    }, [count])
+
+    const variants = {
+        hidden: {
+            // y: 50,
+            opacity: 0
+        },
+        visible: {
+            // y: 0,
+            opacity: 1
+        }
+    }
+
     return (
         <Container padding="60px 0" margin="0">
             <ContentContainer>
-                {weAre.map((item, index) =>
-                    <Paragraph key={index} fontSize="36px" fontWeight="500">{item.content.text}</Paragraph>
-                )}
+                <Paragraph fontSize="36px" fontWeight="500" >We are
+                <AnimatePresence>
+                        <motion.span
+                            variants={variants}
+                            initial="hidden"
+                            animate="visible"
+                            exit="hidden"
+                        // key={count}
+                        > {weAre[count].content.text}</motion.span>
+                    </AnimatePresence>
+                </Paragraph>
             </ContentContainer>
         </Container>
     )
@@ -17,6 +52,9 @@ const WeAreSection = ({ weAre }) => {
 export default WeAreSection
 
 const ContentContainer = styled.div`
+    display: flex;
+    align-items: center;
     max-width: 959px;
+    min-height: 300px;
     margin: 0 auto;
 `;

ad4927f8c981381efe3e3c20b997558bda7ca3b9
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-04-22 00:40:52 -1000

    feat: adds SDG site meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sae Design Group`,
+    description: `Delightful design by good people.`,
+    author: `@sdghi`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

2f96cda48b3bbe0fe6acd88c51d25cf23dfceb30
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-12-20 22:53:50 -1000

    edit: refactors custom cursor on related projects

--- src/components/cursors/relatedProjectsCursor.js
+++ src/components/cursors/relatedProjectsCursor.js
@@ -1,28 +1,62 @@
-import React from "react"
+import React, { useState, useRef, useEffect } from "react"
 import { Cursor } from "../../elements"
 import styled from "styled-components"
-import { white, black } from "../../variables"
+import { white, pink } from "../../variables"
 
 const RelatedProjectCursor = ({ xValue, yValue, cursorContent }) => {
+  const relatedRef = useRef(null)
+  const [measurements, setMeasurements] = useState({ height: 0, width: 0 })
+
+  useEffect(() => {
+    const { clientHeight, clientWidth } = relatedRef.current
+
+    setMeasurements({
+      height: -clientHeight / 2,
+      width: -clientWidth / 2,
+    })
+  }, [cursorContent])
+
   return (
     <RelatedProjects
       top={yValue}
       left={xValue}
-      height="200"
-      width="200"
+      height={measurements.height + "px"}
+      width={measurements.width + "px"}
+      ref={relatedRef}
       zIndex="999999"
       cursorContent={cursorContent}
-      dangerouslySetInnerHTML={{ __html: cursorContent }}
-    />
+    >
+      <h4>View</h4>
+      <p>{cursorContent}</p>
+    </RelatedProjects>
   )
 }
 
 export default RelatedProjectCursor
 
 const RelatedProjects = styled(Cursor)`
-  font-size: 36px;
   font-weight: 700;
-  color: ${({ cursorContent }) => cursorContent === '&#x2192;' || cursorContent === '&#x2190;' ? black : white};
+  left: ${({ width }) => width};
+  top: ${({ height }) => height};
+  margin: 0;
+  padding: 20px;
+  border-radius: 20px;
+  max-width: 300px;
   text-align: center;
   line-height: 1;
+  color: ${white};
+  background-color: ${pink};
+
+  p {
+    margin: 0;
+  }
+
+  h4 {
+    margin-top: 0;
+    margin-bottom: 5px;
+  }
+
+  p {
+    font-size: 24px;
+  }
 `

12090c5e8606a9285921206b378e964b561c000f
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-02-04 22:14:51 -1000

    feat: adds home video

--- src/components/home/homeHero.js
+++ src/components/home/homeHero.js
@@ -1,11 +1,39 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
 const HomeHero = () => {
+  const { allPrismicHomepage: homepage } = useStaticQuery(
+    graphql`
+      query MyQuery {
+        allPrismicHomepage {
+          edges {
+            node {
+              data {
+                video {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const { video } = homepage.edges[0].node.data
+
   return (
     <section>
-      <h2>Homepage</h2>
+      <Video autoPlay muted loop playsInline src={video.url} />
     </section>
   )
 }
 
 export default HomeHero
+
+const Video = styled.video`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`

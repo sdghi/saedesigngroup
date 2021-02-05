a261624b2ea427f0300bbe88c3c34fe1670a467c
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-02-04 23:10:44 -1000

    edit: styles the menu button

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import { useCursorChange } from "../hooks"
-import { pink } from "../variables"
+import { yellow, black } from "../variables"
 import { motion } from "framer-motion"
 import SiteBranding from "./siteBranding"
 
@@ -9,28 +9,47 @@ const Header = ({ toggleNav, isNavOpen }) => {
   const [bind] = useCursorChange({ selected: "selected" })
 
   return (
-    <header>
-      <SiteBranding />
+    <SiteHeader>
+      <SiteBranding isNavOpen={isNavOpen} toggleNav={toggleNav} />
       <ToggleBtn onClick={toggleNav} {...bind}>
-        <motion.h2 whileHover={{ scale: 1.2, rotate: 4 }}>
-          {isNavOpen ? "close" : "menu"}
-        </motion.h2>
+        <h2>{isNavOpen ? "Close" : "Menu"}</h2>
       </ToggleBtn>
-    </header>
+    </SiteHeader>
   )
 }
 
 export default Header
 
-const ToggleBtn = styled.div`
+const SiteHeader = styled.header`
   position: fixed;
-  top: 31px;
-  right: 20px;
+  top: 0;
+  left: 0;
   z-index: 99999;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 20px;
+`
+const ToggleBtn = styled(motion.button)`
   font-weight: 600;
-  color: ${pink};
+  color: ${black};
   transition: all 0.01s ease-in;
   user-select: none;
+  background: ${yellow};
+  border: 2px solid ${black};
+  padding: 10px 20px;
+  transition: all 0.1s ease-out;
+  border-radius: 10px;
+
+  &:focus {
+    outline: 0;
+  }
+
+  &:hover {
+    transition: all 0.1s ease-in;
+    transform: rotate(5deg) scale(1.1);
+  }
 
   h2 {
     font-size: 20px;

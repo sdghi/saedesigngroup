15e873e03700444ec3ad319423100dbb270d113f
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-02-09 16:26:16 -1000

    edit: slows down speed of client list marquee

--- src/components/about/aboutQuote.js
+++ src/components/about/aboutQuote.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import styled from "styled-components"
 import { Container, Paragraph, HeadingTwo } from "../../elements"
 import { pink, breakpoint4k } from "../../variables"

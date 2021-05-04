d9277d3b8e8a897f092a044578b35b2ccc1363f1
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-05-04 00:06:16 -1000

    feat: adds scroll down cta

--- src/components/home/homeHero.js
+++ src/components/home/homeHero.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { pink } from "../../variables"
 import styled from "styled-components"
 
 const HomeHero = () => {
@@ -24,16 +25,54 @@ const HomeHero = () => {
   const { video } = homepage.edges[0].node.data
 
   return (
-    <section>
+    <Hero>
       <Video autoPlay muted loop playsInline src={video.url} />
-    </section>
+      <ScrollCta>scroll down</ScrollCta>
+    </Hero>
   )
 }
 
 export default HomeHero
 
+const Hero = styled.section`
+  position: relative;
+`
+
 const Video = styled.video`
   height: 100%;
   width: 100%;
   object-fit: contain;
+`
+
+const ScrollCta = styled.p`
+  position: absolute;
+  bottom: 46px;
+  right: 0;
+  writing-mode: vertical-lr;
+  font-weight: 700;
+
+  &:after {
+    content: "";
+    position: absolute;
+    top: calc(100% + 5px);
+    left: calc(50% - 3px);
+    width: 3px;
+    height: 38px;
+    background: black;
+    animation: bob 2s ease-in-out infinite;
+    transform-origin: top;
+    background: ${pink};
+  }
+
+  @keyframes bob {
+    0% {
+      transform: scaleY(0);
+    }
+    50% {
+      transform: scaleY(1);
+    }
+    100% {
+      transform: scaleY(0);
+    }
+  }
 `
